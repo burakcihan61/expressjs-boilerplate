0795c1af26537fe8030477ccd94e26db72f539b7
Burak Cihan <burakcihan622@gmail.com>
2023-08-13 02:51:39 +0300

    feat: middlewares error

--- src/app.js
+++ src/app.js
@@ -78,20 +78,6 @@ if (config.env !== 'test') {
   );
 }
 
-// error handler, send stacktrace only during development
-app.use(
-  (
-    err,
-    req,
-    res,
-    next, // eslint-disable-line no-unused-vars
-  ) =>
-    res.status(err.status).json({
-      message: err.isPublic ? err.message : httpStatus[err.status],
-      stack: config.env === 'development' ? err.stack : {},
-    }),
-);
-
 // convert error to ApiError, if needed
 app.use(errorConverter);
 
