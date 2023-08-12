70967d49e137531e5937da0f3061cb6e310f2d49
Burak Cihan <burakcihan622@gmail.com>
2023-08-13 02:40:51 +0300

    feat: winston

--- src/config/winston.js
+++ src/config/winston.js
@@ -1,10 +1,24 @@
 const winston = require('winston');
+const config = require('./config');
 
-const logger = new winston.Logger({
+const enumerateErrorFormat = winston.format((info) => {
+  if (info instanceof Error) {
+    Object.assign(info, { message: info.stack });
+  }
+  return info;
+});
+
+const logger = winston.createLogger({
+  level: config.env === 'development' ? 'debug' : 'info',
+  format: winston.format.combine(
+    enumerateErrorFormat(),
+    config.env === 'development' ? winston.format.colorize() : winston.format.uncolorize(),
+    winston.format.splat(),
+    winston.format.printf(({ level, message }) => `${level}: ${message}`)
+  ),
   transports: [
     new winston.transports.Console({
-      json: true,
-      colorize: true,
+      stderrLevels: ['error'],
     }),
   ],
 });
